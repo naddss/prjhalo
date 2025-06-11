866e242f22ff3b677838f0562711e75664e55094
naddss <nad.hilma10@gmail.com>
2025-06-11 09:44:06 +0800

    Menerima masukan jumlah nama

--- hallo.cpp
+++ hallo.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 int main() {
-    string nama[5] = {"Ali", "Budi", "Citra", "Dina", "Eka"};
+    int n;
+    cout << "Jumlah nama: ";
+    cin >> n;
+    cin.ignore();
 
-    cout << "Daftar nama:\n";
-    for (int i = 0; i < 5; i++) {
-        cout << nama[i] << endl;
+    string nama[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Nama ke-" << i+1 << ": ";
+        getline(cin, nama[i]);
     }
+
+    cout << "\nDaftar nama:\n";
+    for (int i = 0; i < n; i++)
+        cout << nama[i] << endl;
 
     return 0;
 }
-
 
